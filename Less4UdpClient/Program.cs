543dedc11b2d1c143fc3c033fdbae3c48490bcf8
unknown <bekirovvusal53@gmail.com>
2024-10-12 16:12:52 +0300

    Udp File Transfer

---
+++ Less4UdpClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var ipAddress = IPAddress.Parse("192.168.1.82");
+var port = 54751;
+
+var ep = new IPEndPoint(ipAddress, port);
+
+var client = new UdpClient(); 
+
+while (true)
+{
+    var bytes = new byte[64000];
+    int len = 0;
+
+    using(var fs = new FileStream("C:\\Users\\DELL\\Desktop\\table-hw (1).png", FileMode.Open, FileAccess.Read))
+    {
+        while((len = fs.Read(bytes, 0, bytes.Length)) > 0)
+        {
+            client.Send(bytes,len,ep);
+        }
+
+    }
+    Console.WriteLine("Process completed successfully");
+
+    Console.ReadKey();
+}
---
+++ UpdServer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var ipAddress = IPAddress.Parse("192.168.1.82");
+var port = 54751; ;
+
+var ep = new IPEndPoint(ipAddress, port);
+
+var server = new UdpClient(ep);
+
+
+var remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+
+while (true)
+{
+
+    var info =  Directory.CreateDirectory("C:\\Users\\DELL\\Desktop\\ServerUdp");
+    var bytes = new byte[64000];
+    var dInfo = Directory.CreateDirectory($"{info.FullName}\\FolderForUdp");
+    int len;
+
+    using (var fs = new FileStream($"{dInfo.FullName}\\Image.png", FileMode.OpenOrCreate, FileAccess.Write))
+    {
+        while ((len = (bytes = server.Receive(ref remoteEndPoint)).Length) > 0)
+        {
+            fs.Write(bytes, 0, len);
+        }
+    }
+
+    Console.WriteLine("Process completed");
+
+    Console.ReadKey();
+}
